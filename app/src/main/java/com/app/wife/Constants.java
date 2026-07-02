502ce3cc58eca598ed24afca5a23e9a51eafa98b
josyvine <64621103+josyvine@users.noreply.github.com>
2026-07-02 12:48:58 +0530

    Update Constants.java

--- app/src/main/java/com/app/wife/Constants.java
+++ app/src/main/java/com/app/wife/Constants.java
@@ -14,6 +14,9 @@ public final class Constants {
     public static final int OFF_PORT_GROUP_AUDIO = 8903;
     public static final int OFF_PORT_GROUP_VIDEO = 8904;
 
+    // Decoupled group file transfer TCP port
+    public static final int OFF_PORT_GROUP_FILE = 8905;
+
     // Intents and extras keys
     public static final String EXTRA_PEER_IP = "com.wife.app.EXTRA_PEER_IP";
     public static final String EXTRA_PEER_NAME = "com.wife.app.EXTRA_PEER_NAME";
@@ -37,6 +40,9 @@ public final class Constants {
     public static final String SIGNAL_GROUP_CALL_REJECT = "GROUP_CALL_REJECT";
     public static final String SIGNAL_GROUP_CALL_END = "GROUP_CALL_END";
 
+    // Peer Roster synchronization signal for multi-device transfers
+    public static final String SIGNAL_PEER_ROSTER_SYNC = "PEER_ROSTER_SYNC";
+
     // Broadcast actions
     public static final String ACTION_CONNECTION_CHANGED = "com.wife.app.ACTION_CONNECTION_CHANGED";
     public static final String ACTION_PEER_DISCOVERED = "com.wife.app.ACTION_PEER_DISCOVERED";
@@ -59,6 +65,17 @@ public final class Constants {
     public static final String ACTION_TRANSFER_PROGRESS = "com.wife.app.ACTION_TRANSFER_PROGRESS";
     public static final String ACTION_TRANSFER_COMPLETE = "com.wife.app.ACTION_TRANSFER_COMPLETE";
     public static final String ACTION_TRANSFER_ERROR = "com.wife.app.ACTION_TRANSFER_ERROR";
+
+    // --- Decoupled Group File Transfer Actions ---
+    public static final String ACTION_GROUP_START_TRANSFER = "com.wife.app.ACTION_GROUP_START_TRANSFER";
+    public static final String ACTION_GROUP_PAUSE_TRANSFER = "com.wife.app.ACTION_GROUP_PAUSE_TRANSFER";
+    public static final String ACTION_GROUP_RESUME_TRANSFER = "com.wife.app.ACTION_GROUP_RESUME_TRANSFER";
+    public static final String ACTION_GROUP_CANCEL_TRANSFER = "com.wife.app.ACTION_GROUP_CANCEL_TRANSFER";
+
+    // --- Decoupled Group File Transfer Progress Broadcast Actions ---
+    public static final String ACTION_GROUP_TRANSFER_PROGRESS = "com.wife.app.ACTION_GROUP_TRANSFER_PROGRESS";
+    public static final String ACTION_GROUP_TRANSFER_COMPLETE = "com.wife.app.ACTION_GROUP_TRANSFER_COMPLETE";
+    public static final String ACTION_GROUP_TRANSFER_ERROR = "com.wife.app.ACTION_GROUP_TRANSFER_ERROR";
 
     // --- File Transfer Progress Extras Keys ---
     public static final String EXTRA_BYTES_TRANSFERRED = "com.wife.app.EXTRA_BYTES_TRANSFERRED";
